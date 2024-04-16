44996b254b0d7e49e96fb539a8ec8b7fecdc609d
dkottel <162588115+dkottel@users.noreply.github.com>
2024-04-16 20:14:16 +0000

    Resolução

--- exercicios/4-refinando-consulta/2-group-by-e-having.sql
+++ exercicios/4-refinando-consulta/2-group-by-e-having.sql
@@ -1,7 +1,32 @@
 -- Crie uma consulta usando as tabelas "invoices" e "customers" cujo o resultado seja o total de gastos por cliente
-
+SELECT
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+SUM(inv.Total) AS total_gasto
+FROM
+invoices AS inv
+INNER JOIN customers AS cus ON inv.CustomerId = cus.CustomerId
+GROUP BY id_cliente;
 
 -- Na consulta anterior, insira um filtro para que apresente apenas os clientes que gastaram mais de R$30
-
+SELECT
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+SUM(inv.Total) AS total_gasto
+FROM
+invoices AS inv
+INNER JOIN customers AS cus ON inv.CustomerId = cus.CustomerId
+GROUP BY id_cliente
+HAVING SUM(inv.Total) >= 30;
 
 -- Na consulta anterior, insira uma coluna que informe a compra de maior valor feita por cada cliente
+SELECT
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+SUM(inv.Total) AS total_gasto,
+MAX(inv.Total) AS compra_maior_valor
+FROM
+invoices AS inv
+INNER JOIN customers AS cus ON inv.CustomerId = cus.CustomerId
+GROUP BY id_cliente
+HAVING SUM(inv.Total) >= 30;
